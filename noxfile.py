9a6ccc36287b00a7b5cea2415ab36e9ef6d63aba
Daniel Obraczka <daniel@obraczka.de>
2023-08-30 21:20:03 +0200

    Try different installation for tests in nox

--- noxfile.py
+++ noxfile.py
@@ -4,11 +4,7 @@ from nox_poetry import Session, session
 @session()
 def tests(session: Session) -> None:
     args = session.posargs or ["--cov", "--cov-report=xml"]
-    session.install(".")
-    session.install("strawman")
-    session.install("pytest")
-    session.install("pytest-cov")
-    session.install("pytest-mock")
+    session.run_always("poetry", "install", external=True)
     session.run("pytest", *args)
 
 
